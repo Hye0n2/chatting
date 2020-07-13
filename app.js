219609e0d14c0484294912d4ee589c25997bbe70
sh <koem204@naver.com>
2020-07-13 16:06:32 +0900

    chatting

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.set('views', `${__dirname}/public`);
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static('src', {extensions: ['html']}));
+app.use(express.static('public', {extensions: ['html']}));
+
+
+io.on ('connection',(socket)=>{
+    console.log('User connected...');
+    socket.on('chatting', (data)=>{
+        io.sockets.emit('chatting', data);
+    })
+})
 
 app.get('/', (req,res) =>{
+    console.log(__dirname);
     res.render('index');
 })
 app.get('/api/users', (req,res)=>{
@@ -17,6 +28,8 @@ app.get('/api/users', (req,res)=>{
     res.send(users);
 })
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`);
-})
+// app.listen(PORT, ()=>{
+//     console.log(`server is running on ${PORT}`);
+// })
+
+http.listen(PORT,()=> console.log(`listening on localhost:${PORT}`));
