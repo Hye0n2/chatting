2bbf9d3eb1e4edc548022485d2244d8edae4c807
sh <koem204@naver.com>
2020-07-13 12:03:24 +0900

    chatting server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set('views', `${__dirname}/public`);
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('src', {extensions: ['html']}));
+
+app.get('/', (req,res) =>{
+    res.render('index');
+})
+app.get('/api/users', (req,res)=>{
+    const users = [
+        {name: "Hong", age: 25}
+    ];
+    res.send(users);
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`);
+})
